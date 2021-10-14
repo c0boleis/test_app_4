c72f277f4b54adf34b59e20023896a46328751e4
Corentin Boleis <corentinboleis@hotmail.fr>
2021-10-15 00:32:17 +0200

    add view, and manage bootstrap conf

---
+++ src/main/webapp/app/gantt/gantt-view/gantt-view.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-gantt-view',
+  templateUrl: './gantt-view.component.html',
+  styleUrls: ['./gantt-view.component.scss'],
+})
+export class GanttViewComponent {}
--- src/main/webapp/app/gantt/gantt.module.ts
+++ src/main/webapp/app/gantt/gantt.module.ts
@@ -4,9 +4,10 @@ import { GanttToolsComponent } from './gantt-tools/gantt-tools.component';
 import { GanttMainComponent } from './gantt-main/gantt-main.component';
 import { RouterModule } from '@angular/router';
 import { ganttState } from './gantt.route';
+import { GanttViewComponent } from './gantt-view/gantt-view.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(ganttState)],
-  declarations: [GanttToolsComponent, GanttMainComponent],
+  declarations: [GanttToolsComponent, GanttMainComponent, GanttViewComponent],
 })
 export class GanttModule {}
