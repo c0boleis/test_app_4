9465da2a94b6b69ef8e3d3ba0b2bfdf1445015bb
bat <corentinboleis@hotmail.fr>
2021-11-26 05:35:13 +0100

    correction update data

--- src/main/webapp/app/gantt/gantt-object/gantt-object.component.ts
+++ src/main/webapp/app/gantt/gantt-object/gantt-object.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, ElementRef, Output, Attribute, OnInit } from '@angular/core';
-import { Task } from '../../entities/task/task.model';
+import { ITask, Task } from '../../entities/task/task.model';
 import * as dayjs from 'dayjs';
+import { CdkDragEnd } from '@angular/cdk/drag-drop';
+import { TaskService } from 'app/entities/task/service/task.service';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { finalize } from 'rxjs/internal/operators/finalize';
 
 @Component({
   selector: 'jhi-gantt-object',
@@ -8,18 +13,22 @@ import * as dayjs from 'dayjs';
   styleUrls: ['./gantt-object.component.scss'],
 })
 export class GanttObjectComponent implements OnInit {
-  //mont 0-11
-  //day 1-31
+  // mont 0-11
+  // day 1-31
   refDate: dayjs.Dayjs = dayjs(new Date(2021, 9, 14));
 
-  @Output()
-  dragPosition = { x: 0, y: 0 };
+  isSaving = false;
+
+  // @Input()
+  freeDragPosition = { x: 200, y: 0 };
 
   @Input()
   task?: Task;
 
+  constructor(private taskService: TaskService) {}
+
   ngOnInit(): void {
-    this.dragPosition = { x: this.getLeft(), y: 0 };
+    this.freeDragPosition = { x: this.getLeft(), y: 0 };
   }
 
   getLeft(): number {
@@ -28,5 +37,47 @@ export class GanttObjectComponent implements OnInit {
       return delta;
     }
     return 0;
+  }
+
+  updateStartTime(data: CdkDragEnd): void {
+    if (this.task !== undefined) {
+      this.freeDragPosition.x = this.freeDragPosition.x + data.distance.x;
+      if (this.task.startTime !== undefined && this.task.startTime !== null) {
+        console.warn('old date: ' + this.task.startTime.toString());
+      }
+      this.task.startTime = this.refDate.add(this.freeDragPosition.x, 'day');
+      console.warn(`delta time: ${this.freeDragPosition.x}`);
+      console.warn('new date: ' + this.task.startTime.toString());
+      const taskTmp: Task = new Task();
+      taskTmp.id = this.task.id;
+      taskTmp.taskName = this.task.taskName;
+      taskTmp.startTime = this.refDate.add(this.freeDragPosition.x, 'day');
+      taskTmp.endTime = this.task.endTime;
+
+      this.subscribeToSaveResponse(this.taskService.update(taskTmp));
+    }
+  }
+
+  protected subscribeToSaveResponse(result: Observable<HttpResponse<ITask>>): void {
+    result.pipe(finalize(() => this.onSaveFinalize())).subscribe(
+      () => this.onSaveSuccess(),
+      () => this.onSaveError()
+    );
+  }
+
+  protected previousState(): void {
+    window.history.back();
+  }
+
+  protected onSaveSuccess(): void {
+    //TODO
+  }
+
+  protected onSaveError(): void {
+    // Api for inheritance.
+  }
+
+  protected onSaveFinalize(): void {
+    this.isSaving = false;
   }
 }
